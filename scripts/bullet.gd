01b653d0dd4196454194cc56913122d03499f39c
iIInfernoDashIi <trololochkabyminecraft@gmail.com>
2023-04-03 20:19:53 +0700

    Генератор пуль

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,21 +1,26 @@
 extends Area2D
 
 
-@export var speed: int = 100
-@export var velocit: Vector2 = Vector2(0,0)
-@export var angle: int = 0
+@export var speed: int = 125
+@export var lifeTime: float = 10
+
+@onready var velocity: Vector2 = Vector2(0,1)
 
 
 func _ready():
-	rotation_degrees = angle
+	$LifeTime.wait_time = lifeTime
+	$LifeTime.start()
 
 
 func _process(delta):
-	var velocity = Vector2 (0, -1).rotated(rotation)
-	position += velocity * speed * delta
+	position += velocity.rotated(rotation) * speed * delta
 
 
 func _on_Bullet_body_entered(body):
 	if body.name == "Player":
 		body.take_damage()
 		queue_free()
+
+
+func _on_life_time_timeout():
+	queue_free()
